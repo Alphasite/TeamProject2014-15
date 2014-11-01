1605e600234c5685b3ef48e630b81e3bc1c7894c
Nishad Mathur <nishadmathur@gmail.com>
2014-11-01 18:07:37 +0000

    Created a new test grammar for EBNF. There is a know issue with '|' rules, where the parser will not recognise them, but i am working on it atm. This is only a proof of concept, and just to see roughly hwo we should structure our initial grammar and decide upon its features.

---
+++ Plugin/src/uk/ac/gla/teamL/EBNFParserUtil.java
@@ -0,0 +1,11 @@
+package uk.ac.gla.teamL;
+
+import com.intellij.lang.parser.GeneratedParserUtilBase;
+
+/**
+ * User: nishad
+ * Date: 01/11/14
+ * Time: 16:58
+ */
+public class EBNFParserUtil extends GeneratedParserUtilBase {
+}
