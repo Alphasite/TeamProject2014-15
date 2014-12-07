61b99d42a27d5b5f118e0d5804dad60f2160410a
Nishad Mathur <nishadmathur@gmail.com>
2014-12-07 21:34:49 +0000

    New icon.

--- Plugin/src/uk/ac/gla/teamL/EBNFIcon.java
+++ Plugin/src/uk/ac/gla/teamL/EBNFIcon.java
@@ -10,6 +10,6 @@ import javax.swing.*;
  * Time: 16:52
  */
 public class EBNFIcon {
-    public static final Icon FILE = IconLoader.getIcon("/Assets/Icon/TeamProjectIcon16MK2.png");
+    public static final Icon FILE = IconLoader.getIcon("/Assets/Icon/eBNFIcon13.png");
     public static final Icon RECURSIVE = IconLoader.getIcon("/Assets/Icon/RecursiveMarker13.png");
 }
