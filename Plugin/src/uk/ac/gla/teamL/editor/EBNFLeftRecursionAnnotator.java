07a6011659e4be6a6910869534f7df2d7dece323
Nishad Mathur <nishadmathur@gmail.com>
2014-12-13 02:12:49 +0800

    Fixed Left Recursion.

--- Plugin/src/uk/ac/gla/teamL/editor/EBNFLeftRecursionAnnotator.java
+++ Plugin/src/uk/ac/gla/teamL/editor/EBNFLeftRecursionAnnotator.java
@@ -7,6 +7,7 @@ import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import uk.ac.gla.teamL.parser.psi.EBNFAssignment;
 import uk.ac.gla.teamL.parser.psi.EBNFIdentifier;
+import uk.ac.gla.teamL.parser.psi.EBNFNestedRules;
 import uk.ac.gla.teamL.parser.psi.EBNFRuleElement;
 
 import java.util.List;
@@ -28,9 +29,23 @@ public class EBNFLeftRecursionAnnotator implements Annotator {
 
             if (subRules.size() > 0) {
                 PsiElement deepest = PsiTreeUtil.getDeepestFirst(subRules.get(0));
-                EBNFIdentifier leftMostRule = PsiTreeUtil.getParentOfType(deepest, EBNFIdentifier.class);
 
-                if (leftMostRule != null && leftMostRule.getName() != null) {
+                // Handle the corner case of nested rules,
+                // the only place where an id can be and also not be the deepest element.
+                deepest = deepest.getParent();
+
+                while (deepest instanceof EBNFNestedRules) {
+                    deepest = PsiTreeUtil.getDeepestFirst(((EBNFNestedRules) deepest).getRules());
+                }
+
+                EBNFIdentifier leftMostRule;
+                if (!(deepest instanceof EBNFIdentifier)) {
+                    leftMostRule = PsiTreeUtil.getParentOfType(deepest, EBNFIdentifier.class);
+                } else {
+                    leftMostRule = (EBNFIdentifier) deepest;
+                }
+
+                if (leftMostRule != null) {
                     if (leftMostRule.getName().equals(name)) {
                         annotationHolder.createWarningAnnotation(leftMostRule, "Rule is left recursive. This may cause issues with certain parser generators.");
                     }
