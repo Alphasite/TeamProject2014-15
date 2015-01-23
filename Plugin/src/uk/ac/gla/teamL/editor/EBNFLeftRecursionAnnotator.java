01fe1718a2c32c1d1c09349e8a228842db3925a4
Bryan <bryanmdocherty@gmail.com>
2015-01-23 13:28:17 +0000

    Indirect Left Recursion Detection
    Each assignment is passed to an annotator. If the assignment is left recursive then the annotator highlights the code for the that assignment.
    
    BUG FIXED:
    Annotator would get stuck in an infinite loop when a non-recursive assignment pointed to a recursive assignment.
    Annotator now knows which assignments it has looked at to prevent it getting stuck in a loop.

--- Plugin/src/uk/ac/gla/teamL/editor/EBNFLeftRecursionAnnotator.java
+++ Plugin/src/uk/ac/gla/teamL/editor/EBNFLeftRecursionAnnotator.java
@@ -1,28 +1,14 @@
 package uk.ac.gla.teamL.editor;
 
-import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
-import com.intellij.openapi.editor.SyntaxHighlighterColors;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiLiteralExpression;
 import com.intellij.psi.PsiWhiteSpace;
-import com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl;
-import com.intellij.psi.search.searches.ReferencesSearch;
-import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import uk.ac.gla.teamL.parser.psi.EBNFAssignment;
 import uk.ac.gla.teamL.parser.psi.EBNFIdentifier;
 import uk.ac.gla.teamL.parser.psi.EBNFRuleElement;
-import uk.ac.gla.teamL.parser.psi.impl.EBNFIdentifierImpl;
-
-import javax.naming.Reference;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Stack;
 
@@ -39,16 +25,18 @@ public class EBNFLeftRecursionAnnotator implements Annotator {
         }
     }
 
-    private boolean isLeftRecursive(EBNFAssignment assignment) {
+    private static boolean isLeftRecursive(EBNFAssignment assignment) {
+
+        ArrayList<String> visited = new ArrayList<String>();
 
         String name = assignment.getName();
 
         Stack<EBNFAssignment> stack = new Stack<EBNFAssignment>();
         stack.push(assignment);
+        visited.add(assignment.getName());
 
         while (!stack.empty()) {
             EBNFAssignment poppedAssignment = stack.pop();
-//           try {
             ArrayList<String> identifiers = getIdentifiers(poppedAssignment);
 
             for (String identifier: identifiers) {
@@ -57,29 +45,19 @@ public class EBNFLeftRecursionAnnotator implements Annotator {
                     return true;
                 } else {
                     EBNFAssignment idAssignment = getAssignment(identifier, poppedAssignment);
-                    if (idAssignment != null) {
+                    if (idAssignment != null && !(visited.contains(idAssignment.getName()))) {
                         stack.push(idAssignment);
+                        visited.add(idAssignment.getName());
                     }
                 }
             }
-//           } catch (NullPointerException e) {
-//               System.err.println("--------------------------------------------------");
-//               System.err.println("NUll POINTER EXCEPTION");
-//               System.err.println("ASSIGNMENT == NULL?");
-//               if (poppedAssignment == null) {
-//                   System.err.println("TRUE");
-//               } else System.err.println("FALSE");
-//               System.err.println("--------------------------------------------------");
-//           }
         }
-        PsiTreeUtil.getChildrenOfType(assignment, EBNFIdentifier.class);
         return false;
     }
 
-    private ArrayList<String> getIdentifiers(EBNFAssignment assignment) {
+    private static ArrayList<String> getIdentifiers(EBNFAssignment assignment) {
         ArrayList<String> identifiers = new ArrayList<String>();
 
-//        try {
             List<EBNFRuleElement> rules = assignment.getRuleElementList();
             for (EBNFRuleElement rule : rules) {
                 PsiElement child = rule.getFirstChild();
@@ -94,16 +72,10 @@ public class EBNFLeftRecursionAnnotator implements Annotator {
                     }
                 }
             }
-//        } catch (NullPointerException e) {
-//            System.err.println("--------------------------------------------------");
-//            System.err.println("NUll POINTER EXCEPTION");
-//            System.err.println("ASSIGNMENT-> " + assignment.getName());
-//            System.err.println("--------------------------------------------------");
-//        }
         return identifiers;
     }
 
-    private EBNFAssignment getAssignment(String identifier, EBNFAssignment assignment) {
+    private static EBNFAssignment getAssignment(String identifier, EBNFAssignment assignment) {
         PsiElement psiElement = assignment.getPrevSibling();
         while (psiElement != null) {
             if (psiElement instanceof EBNFAssignment) {
@@ -127,21 +99,3 @@ public class EBNFLeftRecursionAnnotator implements Annotator {
     }
 
 }
-
-//    private boolean isLeftRecursive(String name, EBNFAssignment assignment) {
-//        ArrayList<String> identifiers = getIdentifiers(assignment);
-//
-//        boolean recursive = false;
-//        for (String identifier: identifiers) {
-//            if (identifier.equals(name)) {
-//                recursive = true;
-//            } else {
-//                EBNFAssignment idAssignment = getAssignment(identifier, assignment);
-//                if (idAssignment == null) { System.out.println("--> BREAK"); break; }
-//                if (isLeftRecursive(name, idAssignment)) {
-//                    recursive = true;
-//                }
-//            }
-//        }
-//        return recursive;
-//    }
